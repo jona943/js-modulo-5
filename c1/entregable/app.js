0d0c69486e1a2ab2c9a347139c9ef32b174936ab
Jonathan <jonathan603015@gmail.com>
2026-04-22 17:18:11 -0700

    Uso de Promise y setTimeout

--- c1/entregable/app.js
+++ c1/entregable/app.js
@@ -23,7 +23,17 @@ function updateOrderStatus(order, status) {
     }
 }
 
+
 async function processOrder(order) {
     // TODO: Simular la preparación del pedido usando setTimeout y Promise
     // TODO: Actualizar el estado del pedido a "Completado"
+    const tiempoAleatorio = Math.floor(Math.random() * 6000) + 2000;
+
+    await new Promise((resolve) => { // Creamos promesa que espera await a que se resuelva
+        setTimeout(() => { // simulacion de tiempo setTimeout
+            resolve(); // Cuando pasa el tiempo de espera resuelve la promesa
+        }, tiempoAleatorio); // 5 segundo de espera en la preparacion de cada cafe 
+    });
+    updateOrderStatus(order, "Completado"); 
+    // Una vez que se resuleve la promesa se actualiza el estado
 }
